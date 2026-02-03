329453973b4e9b99efab7f26744ca8d77332464b
ko-inseoklee <tjrkd222@kikii.com>
2026-02-03 23:40:33 +0900

    리마인드미 프로젝트 구성

---
+++ remind_me_server/src/main/java/com/example/remind_me_server/RemindMeServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.remind_me_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemindMeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemindMeServerApplication.class, args);
+	}
+
+}
