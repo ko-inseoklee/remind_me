b01c94036e294628ac2c9f3c81610040450ee39b
ko-inseoklee <tjrkd222@gmail.com>
2026-02-17 23:18:46 +0900

    baseEntitywith id 해시코드 및 equal함수 오버라이딩

--- remind_me_server/src/main/java/com/example/common/entity/BaseEntityWithId.java
+++ remind_me_server/src/main/java/com/example/common/entity/BaseEntityWithId.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
+
 /**
  * ID를 가진 엔티티의 기본 클래스
  * Long 타입의 ID를 제공
@@ -21,4 +23,21 @@ public abstract class BaseEntityWithId extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof BaseEntityWithId)) return false;
+        BaseEntityWithId that = (BaseEntityWithId) o;
+        // ID가 null인 경우(영속화 전)에는 동등하지 않다고 판단하거나,
+        // 비즈니스 키가 있다면 그것을 사용해야 함. 여기서는 ID 기반 비교.
+        // 프록시 객체의 필드에 직접 접근하면 null일 수 있으므로 getter 사용 필수
+        return id != null && id.equals(that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        // 영속화 전후 해시코드 일관성을 위해 상수를 반환 (JPA 엔티티 권장 패턴)
+        return getClass().hashCode();
+    }
 }
