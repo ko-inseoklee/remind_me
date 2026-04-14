e43cf02d725876cc3f30c7fddd602f445e11dc30
ko-inseoklee <tjrkd222@gmail.com>
2026-04-14 23:04:39 +0900

    질문 생성 API 개발

--- remind_me_server/src/main/java/com/example/remind_me_server/study/infra/api/QuestionController.java
+++ remind_me_server/src/main/java/com/example/remind_me_server/study/infra/api/QuestionController.java
@@ -8,6 +8,7 @@ import com.example.remind_me_server.global.web.dto.ApiResponse;
 import com.example.remind_me_server.study.application.port.in.CreateQuestionCommand;
 import com.example.remind_me_server.study.application.service.CreateQuestionService;
 import com.example.remind_me_server.study.domain.Question;
+import com.example.remind_me_server.study.infra.api.dto.CreateQuestionRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -26,8 +27,11 @@ public class QuestionController {
     @PostMapping("")
     public ApiResponse<Long> createQuestion(
         @LoginUser long userId,
-        @RequestBody CreateQuestionCommand command
+        @RequestBody CreateQuestionRequest request
     ) {
+
+        CreateQuestionCommand command = request.toCommand(userId);
+
         Question q =  createQuestionService.create(command);
         return ApiResponse.success(q.id(), "생성 성공", HttpStatus.CREATED.value());
     }
